7e9d6a3155447b02774194cd8c43cc3deaf17f65
Keita <fast.break.baske03@gmail.com>
2022-08-15 16:07:10 +0900

    update: App コンポーネントを修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+import { FC } from 'react'
+import { BadgeCheckIcon } from '@heroicons/react/solid'
+import Layout from './components/Layout'
+
+const App: FC = () => {
+  return (
+    <Layout>
+      <p className='my-5 text-xl font-bold'>React18 Lesson</p>
+      <BadgeCheckIcon className='h-12 w-12 text-blue-500' />
+    </Layout>
+  )
 }
 
 export default App
