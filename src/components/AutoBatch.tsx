47a501de0c9cd06a5131eafe64d82e2ba4d1391c
Keita <fast.break.baske03@gmail.com>
2022-08-15 16:06:46 +0900

    add: AutoBatch コンポーネントを作成

---
+++ src/components/AutoBatch.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import Layout from './Layout'
+
+const AutoBatch: FC = () => {
+  return (
+    <Layout>
+      <p>AutoBatch</p>
+    </Layout>
+  )
+}
+
+export default AutoBatch
