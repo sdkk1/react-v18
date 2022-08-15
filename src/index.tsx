7c1f95bcc365797b7c929c97a78b3914dd102255
Keita <fast.break.baske03@gmail.com>
2022-08-15 16:08:38 +0900

    update: ルーティング、React Query の設定を追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import AutoBatch from './components/AutoBatch'
+import './index.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      suspense: true,
+    },
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<App />}
+          />
+          <Route
+            path='/autobatch'
+            element={<AutoBatch />}
+          />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
