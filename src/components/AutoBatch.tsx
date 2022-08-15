96dab4cebcec654aac80b90519fe70d62e1b0590
Keita <fast.break.baske03@gmail.com>
2022-08-16 00:17:39 +0900

    feat: Automatic batching の動作確認

--- src/components/AutoBatch.tsx
+++ src/components/AutoBatch.tsx
@@ -1,10 +1,45 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import axios from 'axios'
+// import { flushSync } from 'react-dom'
 import Layout from './Layout'
 
 const AutoBatch: FC = () => {
+  const [fetchCount, setFetchCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  const clickHandler = async () => {
+    await axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        setUsers(res.data)
+        setFetchCount((fetchCount) => fetchCount + 1)
+
+        // NOTE: 自動バッチングからオプトアウトするためには flushSync を使う
+        // flushSync(() => {
+        //   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        //   setUsers(res.data)
+        // })
+        // flushSync(() => {
+        //   setFetchCount((count) => count + 1)
+        // })
+      })
+  }
+
+  console.log('Rendered AutoBach')
+
   return (
     <Layout>
-      <p>AutoBatch</p>
+      <p className='my-3 text-xl font-bold text-blue-500'>Automatic batching</p>
+      <p className='my-5'>{fetchCount}</p>
+      <p className='my-5'>{users.length}</p>
+      <button
+        className='my-5 rounded bg-indigo-600 px-3 py-1 text-white hover:bg-indigo-500'
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onClick={clickHandler}
+      >
+        click
+      </button>
     </Layout>
   )
 }
